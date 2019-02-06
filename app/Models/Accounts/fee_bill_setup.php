d17b66e4917fcfe445b71efb0af97b6ceaf950ad
ateeb1 <m.ateeb@generations.edu.pk>
2019-02-06 11:09:44 +0500

    fee bill setup updated

---
+++ app/Models/Accounts/fee_bill_setup.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models\Accounts;
+
+use Illuminate\Database\Eloquent\Model;
+
+class fee_bill_setup extends Model
+{
+    protected $connection = 'mysql_Career_fee_bill';
+
+    public function getInstallmentNumberOfMonths(){
+        $data=fee_bill_setup::select('installment_number_of_months')->first();
+        return $data['installment_number_of_months'];
+    }
+
+}
