2475e82473adcbf57f3a372c57a1d81ceb10a01a
ateeb1 <m.ateeb@generations.edu.pk>
2019-05-27 13:42:25 +0500

    absentia cases work

--- public/metronic/global/scripts/attendance_graph.js
+++ public/metronic/global/scripts/attendance_graph.js
@@ -13,23 +13,98 @@ var dailyReport = function(date,staffID){
        url:url+'/masterLayout/getDailyReport',
        success:function(e){
         var connectPayRoll=[false];
-
        $('.noShow').removeClass('noShow');
        var data = JSON.parse(e);
+
        if(data.length != 0){
       
 
            var expected_time = timeToMinute(data[0].day_time_in);
-           var first_tap = timeToMinute(data[0].tap_min);
            
+                  if(data[0].ab_rec_time!="" && data[0].tap_min!=""){
 
-          
+                    var expected_time_out = timeToMinute(data[0].day_time_out);
+                    var expected_time_in = timeToMinute(data[0].day_time_in);
+                    var first_tap = timeToMinute(data[0].tap_min);
+                    var payRollAttendance = data[0].payroll_time_slot;
+                    var ab_actual_tap_in=data[0].ab_actual_tap.split(',')[0];
+                    var ab_actual_tap_out=data[0].ab_actual_tap.split(',')[1];
+                    var ab_actual_tap_in_min=timeToMinute(data[0].ab_actual_tap.split(',')[0]);
+                    var ab_actual_tap_out_min=timeToMinute(data[0].ab_actual_tap.split(',')[1]);
+                    var ab_set_tap_in_his_format=data[0].ab_rec_time.split(',')[0];
+                    var ab_set_tap_out_his_format=data[0].ab_rec_time.split(',')[1];
+                    var ab_set_tap_in=timeToMinute(data[0].ab_rec_time.split(',')[0]);
+                    var ab_set_tap_out=timeToMinute(data[0].ab_rec_time.split(',')[1]);
+                      if(ab_actual_tap_in_min<ab_set_tap_in){
+                          myString=payRollAttendance;
+                          rangestart_timings=payRollAttendance.split(',')[0];
+                          myString = myString.substring(myString.indexOf(',')+1)
+                          myNewPayroll=rangestart_timings+','+ab_actual_tap_in+','+ab_set_tap_in_his_format+','+data[0].day_time_out;
+                          myNewPayroll = myNewPayroll.split(',')
+                          var display_class_sequence=[false,true,true,true,false];
+                          var css_class_name=['green-color','red-color','green-color','red-color'];
+                      }else if(ab_set_tap_out==expected_time_out && ab_set_tap_in!=expected_time_in){
+                          myString=payRollAttendance;
+                          rangestart_timings=payRollAttendance.split(',')[0];
+                          myString = myString.substring(myString.indexOf(',')+1)
+                          myNewPayroll=rangestart_timings+','+rangestart_timings+','+ab_actual_tap_in+','+data[0].day_time_out;
+                          myNewPayroll = myNewPayroll.split(',');
+                          var display_class_sequence=[false,true,true,true,false];
+                          var css_class_name=['red-color','green-color','green-color','grey-color'];
+                      }
+                  }
+if(data[0].ab_rec_time!="" && data[0].tap_min==""){
+          var expected_time_out = timeToMinute(data[0].day_time_out);
+          var expected_time_in = timeToMinute(data[0].day_time_in);
+          var first_tap = timeToMinute(data[0].tap_min);
+          var payRollAttendance = data[0].payroll_time_slot;
+          var ab_actual_tap_in=data[0].ab_actual_tap.split(',')[0];
+          var ab_actual_tap_out=data[0].ab_actual_tap.split(',')[1];
+          var ab_actual_tap_in_min=timeToMinute(data[0].ab_actual_tap.split(',')[0]);
+          var ab_actual_tap_out_min=timeToMinute(data[0].ab_actual_tap.split(',')[1]);
+          var ab_set_tap_in_his_format=data[0].ab_rec_time.split(',')[0];
+          var ab_set_tap_out_his_format=data[0].ab_rec_time.split(',')[1];
+          var ab_set_tap_in=timeToMinute(data[0].ab_rec_time.split(',')[0]);
+          var ab_set_tap_out=timeToMinute(data[0].ab_rec_time.split(',')[1]);
+             if(ab_set_tap_out==expected_time_out && ab_set_tap_in==expected_time_in){
+                  myString=payRollAttendance;
+                  rangestart_timings=payRollAttendance.split(',')[0];
+                  myString = myString.substring(myString.indexOf(',')+1)
+                  myNewPayroll=ab_set_tap_in_his_format+','+ab_set_tap_in_his_format+','+ab_set_tap_out_his_format+','+ab_set_tap_out_his_format;
+                  myNewPayroll = myNewPayroll.split(',');
+                  var display_class_sequence=[false,true,true,true,false];
+                  var css_class_name=['red-color','green-color','red-color','green-color'];
+              }
+              setTimeout(function(){
+                    var  payRollAttendanceArray = timeToMinuteForArray(myNewPayroll);
+                      PayRollAttendanceSlider(payRollAttendanceArray,display_class_sequence,css_class_name,[360,1020]);
+                      },1000);
+          }
+
 
           getLeaveDailyReport(staffID,date,data);
 
            // Weekly Time Sheet
                 var weekly_time_sheet_in = timeToMinute(data[0].day_time_in);
                 var weekly_time_sheet_out = timeToMinute(data[0].day_time_out);
+                setTimeout(function(){
+                  if(data[0].ab_rec_time!="" && data[0].tap_min!=""){
+                    var  payRollAttendanceArray = timeToMinuteForArray(myNewPayroll);
+                      PayRollAttendanceSlider(payRollAttendanceArray,display_class_sequence,css_class_name,[360,1020]);
+                  }
+
+
+                    // PayRollAttendanceSlider(payRollAttendanceArray,[false,true,true,true,false],['green-color','red-color','green-color','red-color'],[360,1020]);
+
+                      },1000);
+
+
+                      
+                      
+                      
+
+                console.log('expected time in'+weekly_time_sheet_in);
+                console.log('expected time out'+weekly_time_sheet_out);
                                   window.FinalTimeOut=data[0].tap_max;
                 var range = [];
                 if(weekly_time_sheet_in && weekly_time_sheet_out){
@@ -214,8 +289,14 @@ var dailyReport = function(date,staffID){
                       // var connectPayRoll = [false];
                       var classPayRoll = [];
                       var payRollAttendance = data[0].payroll_time_slot;
+                      myString=payRollAttendance;
+                      rangestart_timings=payRollAttendance.split(',')[0];
+                      myString = myString.substring(myString.indexOf(',')+1)
+                      console.log(rangestart_timings+','+rangestart_timings+','+myString);
+
                       payRollAttendance = payRollAttendance.split(',')
                       var  payRollAttendanceArray = timeToMinuteForArray(payRollAttendance);
+
                       console.log("payRollAttendanceArrayBefore"+payRollAttendanceArray);
                       var TapCount = data[0].tap_io_num;
 
@@ -776,6 +857,7 @@ var dailyReport = function(date,staffID){
                         }
                         PayRollAttendanceSlider(payRollAttendanceArrayFlexy,connectPayRollFlexy,classPayRollFlexy,range);
                       }
+
                    }else{
 
                     if(Date.parse(date) > Date.parse(data[0].today_date)){
@@ -790,6 +872,7 @@ var dailyReport = function(date,staffID){
 
 
                 }
+                
 
 
 
@@ -1265,6 +1348,8 @@ var dailyReport = function(date,staffID){
     //=======================================================//
 
     var absentiaAllocatedTap = function(tap,connect,range){
+
+      debugger;
        AiAabsentia.noUiSlider.destroy();
        var  aiAabsentia = document.getElementById('AiAabsentia');
        noUiSlider.create(AiAabsentia, {
@@ -1347,6 +1432,7 @@ return window.absentia;
     //=============================================================//
 
     function PayRollAttendanceSlider(payRollAttendanceArray,connectPayRoll,classPayRoll,range){
+      debugger;
            if(payrollSlider != undefined){
             if(payrollSlider.noUiSlider!=undefined){
                               payrollSlider.noUiSlider.destroy();
